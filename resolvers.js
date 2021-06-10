6b8d6cf511b38ad97e1998c10b04aaadec20e4b0
Indrajith V <indrajithvgp@yahoo.com>
2021-06-10 16:15:35 +0530

    optimistic ui added and like functionality added

--- resolvers.js
+++ resolvers.js
@@ -104,6 +104,17 @@ exports.resolvers = {
             return {
                 token: createToken(newUser, process.env.SECRET , '1hr')
             }
-        }
+        },
+        likeRecipe:async(root, {_id, username}, {Recipe, User})=>{
+            const recipe = await Recipe.findOneAndUpdate({_id}, {$inc:{likes: 1} })
+            const user = await User.findOneAndUpdate({username}, {$addToSet:{
+                favorites: _id
+            }})
+            return recipe
+        },
+        deleteUserRecipe:async(root, {_id}, {Recipe})=>{
+            const recipe = await Recipe.findOneAndRemove({_id})
+            return recipe
+        },
     }
 }
